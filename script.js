d2337388feb458a854c3baec623ee54fce2bae39
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-25 10:51:14 -0400

    Consumido API SWAPI e exibido array de planetas no console.log

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 async function planets() {
   let res = await fetch("https://swapi.dev/api/planets?format=json")
 
-  let data = await res.json()
+  let {results} = await res.json()
 
-  console.log(data.results)
+  console.log(results)
 }  
 
