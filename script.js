cc713388d9dbb97a8de2e0a2bb8cce4f7e1dcd3a
MatheusOB21 <matheus53barros@hotmail.com>
2023-05-25 10:59:33 -0400

    Para cada planeta consumido da API, criado um botão

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let planets_ul = document.getElementById('planets')
+
 async function planets() {
+  
   let res = await fetch("https://swapi.dev/api/planets?format=json")
 
   let {results} = await res.json()
-
   console.log(results)
+
+  results.forEach(planet => {
+    let li = document.createElement('li')
+
+    li.innerHTML = `<button>${planet.name}</button>`
+
+    planets_ul.appendChild(li)
+  });
+
 }  
 
